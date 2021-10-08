4dc3ec40c3d9f748e42acf8b6f08e38909bfdeff
Felicia Devina <55679962+feliciadevina22@users.noreply.github.com>
2021-10-08 13:09:33 +0700

    See more category page Lo-Fi

---
+++ AdaSuplai/AdaSuplai/Module/Home/View/Cell/AllCategoryCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  AllCategoryCollectionCell.swift
+//  adaSuplaiTrain
+//
+//  Created by Felicia Devina on 07/10/21.
+//
+
+import UIKit
+
+class AllCategoryCollectionCell: UICollectionViewCell, Identifiable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
