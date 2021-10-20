95712d956a02042b3811e4b343fd59a7bedb5b74
Felicia Devina <55679962+feliciadevina22@users.noreply.github.com>
2021-10-21 00:50:30 +0700

    Category : Category Page & See More Category Clean Code, Name changes AllCategories into SeeMoreCategory

--- AdaSuplai/AdaSuplai/Module/Category/Controller/SeeMoreCategoryController.swift
+++ AdaSuplai/AdaSuplai/Module/Category/Controller/SeeMoreCategoryController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class AllCategoriesController: UIViewController, Identifiable {
+class SeeMoreCategoryController: UIViewController, Identifiable {
     
     @IBOutlet var collectionView: UICollectionView!
     var searchController = UISearchController()
-    var isSearch : Bool = false
+    
+    private enum Constant {
+        static let searchPlaceholder = "Cari Kategori"
+        static var isSearch: Bool = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,48 +27,52 @@ class AllCategoriesController: UIViewController, Identifiable {
     
     // MARK: - Navigation Bar
     
-    private func setUpNavigationBar(){
+    private func setUpNavigationBar() {
         title = "Lihat Lebih"
         
-        if isSearch == false {
+        if Constant.isSearch == false {
+            guard let navigation = self.navigationController else { return }
+            navigation.navigationBar.backgroundColor = .white
+            navigation.navigationBar.barTintColor = .white
+            self.view.backgroundColor = .white
+            self.navigationController?.navigationBar.tintColor = .primaryGreen
+            
             let searchButton = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: .some(#selector(searchTapped(_:))))
             navigationItem.hidesBackButton = false
             navigationItem.hidesSearchBarWhenScrolling = false
             self.navigationItem.titleView = .none
             navigationItem.rightBarButtonItems = [searchButton]
-            self.navigationController?.navigationBar.tintColor = .systemGreen
-            self.navigationController?.navigationBar.backgroundColor = .white
-            self.navigationController?.setNavigationBarHidden(false, animated: false)
         } else {
+            guard let navigation = self.navigationController else { return }
+            navigation.navigationBar.backgroundColor = .white
+            navigation.navigationBar.barTintColor = .white
+            self.view.backgroundColor = .white
+            
             self.navigationItem.titleView = self.searchController.searchBar
             navigationItem.hidesBackButton = true
             navigationItem.hidesSearchBarWhenScrolling = false
             navigationItem.rightBarButtonItems = []
-            self.navigationController?.navigationBar.tintColor = .systemGreen
-            self.navigationController?.navigationBar.backgroundColor = .white
-            self.navigationController?.setNavigationBarHidden(false, animated: false)
         }
         
     }
-
+    
     @objc func searchTapped(_ sender: UIBarButtonItem) {
         print("Search Tapped")
         
-        isSearch = true
+        Constant.isSearch = true
         setUpNavigationBar()
     }
 }
 
-
 // MARK: - Collection View
-extension AllCategoriesController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+extension SeeMoreCategoryController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    func setUpCollectionView(){
-        collectionView.register(AllCategoryCollectionCell.nib(), forCellWithReuseIdentifier: AllCategoryCollectionCell.identifier)
+    func setUpCollectionView() {
+        collectionView.registerNib(forCell: SeeMoreCategoryCollectionCell.self)
         collectionView.contentInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-        collectionView.backgroundColor = .systemGray6
         collectionView.dataSource = self
         collectionView.delegate = self
+        collectionView.backgroundColor = .blueBackground
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -72,29 +80,22 @@ extension AllCategoriesController : UICollectionViewDelegate, UICollectionViewDa
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AllCategoryCollectionCell.identifier, for: indexPath) as? AllCategoryCollectionCell else { return UICollectionViewCell() }
-        cell.layer.cornerRadius = 8
-        cell.backgroundColor = .white
+        let cell = collectionView.dequeueReusableCell(withCell: SeeMoreCategoryCollectionCell.self, for: indexPath)
         return cell
     }
     
+    // CollectionView Dynamic Sizing
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let numRowItems:CGFloat = 2
-        let padding:CGFloat = 16
-        let spacing:CGFloat = 8
+        let numRowItems: CGFloat = 2
+        let padding: CGFloat = 16
+        let spacing: CGFloat = 8
         let width = (collectionView.bounds.width / numRowItems) - padding - spacing
         return CGSize(width: width, height: 100)
     }
 }
 
-
 // MARK: - Search Bar
-extension AllCategoriesController : UISearchControllerDelegate, UISearchBarDelegate  {
-    
-    private enum Constant {
-        static let searchPlaceholder = "Cari Kategori"
-    }
-    
+extension SeeMoreCategoryController: UISearchControllerDelegate, UISearchBarDelegate {
     private func setupSearchController() {
         self.searchController.delegate = self
         self.searchController.searchBar.delegate = self
@@ -107,7 +108,7 @@ extension AllCategoriesController : UISearchControllerDelegate, UISearchBarDeleg
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         print("Cancel")
         
-        isSearch = false
+        Constant.isSearch = false
         setUpNavigationBar()
     }
 }
---
+++ AdaSuplai/AdaSuplai/Module/Category/View/Cell/Category/AllProductCell.swift
@@ -0,0 +1,67 @@
+//
+//  AllProductCell.swift
+//  adaSuplaiTrain2
+//
+//  Created by Felicia Devina on 05/10/21.
+//
+
+import UIKit
+
+class AllProductCell: UITableViewCell, Identifiable {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var mainTableView: UITableView!
+    @IBOutlet var collectionViewHeight: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        setUpCollectionView()
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+}
+// MARK: - Collection View
+
+extension AllProductCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func setUpCollectionView() {
+        collectionView.backgroundColor = .blueBackground
+        collectionView.isScrollEnabled = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.contentInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        self.collectionView.registerNib(forCell: ProductCell.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withCell: ProductCell.self, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == indexPath.last {
+            self.collectionViewHeight.constant = collectionView.contentSize.height + 20
+            print("\(self.collectionViewHeight.constant) == \(collectionView.contentSize.height)")
+            mainTableView.reloadData()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numRowItems: CGFloat = 2
+        let padding: CGFloat = 16
+        let spacing: CGFloat = 4
+        let width = (collectionView.bounds.width / numRowItems) - padding - spacing
+        return CGSize(width: width, height: 305)
+    }
+    
+}
+
+// MARK: - END
