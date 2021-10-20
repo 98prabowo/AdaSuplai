9ce437cae5529375455ebbf0ea5174e4511c5cd0
Felicia Devina <55679962+feliciadevina22@users.noreply.github.com>
2021-10-21 03:51:42 +0700

    Transaction : List Delivery UI, Clean Code

---
+++ AdaSuplai/AdaSuplai/Module/Transaction/View/Cell/DeliveryCell.swift
@@ -0,0 +1,26 @@
+//
+//  DeliveryCell.swift
+//  AdaSuplai
+//
+//  Created by Felicia Devina on 14/10/21.
+//
+
+import UIKit
+
+class DeliveryCell: UITableViewCell, Identifiable {
+    
+    @IBOutlet var deliveryName: UILabel!
+    @IBOutlet var deliveryPrice: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
